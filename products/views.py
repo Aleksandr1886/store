2aa1d7f3111e4acd6f4830a3733df353a892c96b
Aleksandr <saneksem86@gmail.com>
2023-10-18 21:07:18 +0500

    initial commit

---
+++ products/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from products.models import ProductCategory, Product, Basket
+from django.core.paginator import Paginator, EmptyPage
+
+
+def index(request):
+    context = {'title': 'Store'}
+    return render(request, 'products/index.html', context)
+
+
+def products(request, category_id=None, page_number=1):
+    products = Product.objects.filter(category_id=category_id) if category_id else Product.objects.all()
+
+    per_page = 3
+    paginator = Paginator(products, per_page)
+    try:
+        products_paginator = paginator.page(page_number)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+
+    context = {
+        'title': 'Store - Каталог',
+        'categories': ProductCategory.objects.all(),
+        'products': products_paginator,
+    }
+    return render(request, 'products/products.html', context)
+
+
+@login_required
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+@login_required
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
